15bcdc29543ab5bd5b1257dabc4705fa83a68de5
Abi Matthews-Gordon <60277772+Pho3nyxX@users.noreply.github.com>
2024-04-07 16:44:35 -0500

    aside links are working

--- secure-password-designs/scripts/site.js
+++ secure-password-designs/scripts/site.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-
     // load links-container div
     const linksContainer = document.querySelector(".links-container");
 
     // set click listener on links-container
     // show sub-menu div 
     linksContainer.addEventListener("click", (e) => {
-        e.preventDefault();
         let menuItem = e.target.closest('li');
         if (menuItem.classList.contains("menu-item")) {
+            e.preventDefault();
             menuItem.classList.toggle("active");
         }
     });
